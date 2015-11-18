2cdf45329e2b9058098a3bf470a68876a97ce1c9
JustynaStebel <justyna.stebel@gmail.com>
2015-11-18 10:37:05 +0100

    params changed a little bit

--- app/services/facebook_reader.rb
+++ app/services/facebook_reader.rb
@@ -9,8 +9,7 @@ class FacebookReader
     connection = Faraday.new(url: 'https://graph.facebook.com')
 
     params = {
-      :token => @token,
-      :access_token => 'CAACEdEose0cBAFzyXcXmPoZA7KrPuCabzDQxZBnK7CffblZBDnMDdpJvoqTZBQWZAJo56uY1R9R8OTtK8EZBwBPJlZBrnyZBIk03ppUgZChpIZA8LX1VV9UAR26MyPG7e0R5fPaOrSAkQPxnzRZAbwxHP5OFnYQRHly2ZA6fk21FM5JbtQ4uynXEJVZAbvka2BxODD4dDptbhzS2TjAZDZD'
+      :access_token => @token
     }
 
     response = connection.get('/me/posts?fields=message,likes.summary(true),shares',params)
